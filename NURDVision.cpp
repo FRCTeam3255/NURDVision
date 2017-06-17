3c105a5fc3247a576c185d2bf603979e1b5871bb
Tayler Uva <tayleruva@gmail.com>
2017-06-17 00:15:37 -0700

    Cleaned up C++ and added better comments
    Contours still needed

--- NURDVision.cpp
+++ NURDVision.cpp
@@ -14,9 +14,6 @@
 using namespace std;
 using namespace cv;
 
-Mat camera, mask, processed;
-VideoCapture capture(0);
-
 // ========= Constants for Ball tracking ============//
 //Store an array: [0] = lower bound, [1] = upper bound
 double Hue[] = {29.138490103768287, 58.13721981270055};
@@ -24,11 +21,13 @@ double Saturation[] = {64.20863309352518, 255.0};
 double Luminance[] = {75.67446043165468, 255.0};
 // =================================================//
 
-void hslThreshold(Mat &input, double hue[], double sat[], double lum[], Mat &out) {
-		cvtColor(input, out, COLOR_BGR2HLS);
-		inRange(out, Scalar(hue[0], lum[0], sat[0]), Scalar(hue[1], lum[1], sat[1]), out);
+// Converts the image to be within the hslThreshold
+void hslThreshold(Mat &input, double hue[], double sat[], double lum[], Mat &output) {
+		cvtColor(input, output, COLOR_BGR2HLS);
+		inRange(output, Scalar(hue[0], lum[0], sat[0]), Scalar(hue[1], lum[1], sat[1]), output);
 	}
-	
+
+// Creates a mask	
 void createMask(Mat &input, Mat &mask, Mat &output) {
 	mask.convertTo(mask, CV_8UC1);
 	bitwise_xor(output, output, output);
@@ -37,28 +36,36 @@ void createMask(Mat &input, Mat &mask, Mat &output) {
 
 // Does the image processing
 void processImage(Mat& input, Mat& output){
-	hslThreshold(input, Hue, Saturation, Luminance, mask);
-	createMask(input, mask, output);
+	Mat hslOutput, maskOutput;
+	hslThreshold(input, Hue, Saturation, Luminance, hslOutput);
+	createMask(input, hslOutput, maskOutput);
+	output = maskOutput;
 }
 
-// returns true to quit when "ESC" is pressed
+// Returns true to quit when "ESC" is pressed
 bool quit(){
 	int k = waitKey(5);
 	return (k == 27);
 }
 
 int main() {
-		cout << "Start grabbing" << endl
-				<< "Press ESC to terminate" << endl;
-				
-		while (!quit()) {
-			capture.read(camera);
-			processImage(camera, processed);
-			// show output
-//			imshow("input", input);
-			imshow("Processed image", processed);
-		}
-		cout << "Viewer closed";
+	//Creates mats for storing image
+	Mat raw, processed;
+	//Starts video capture of camera 0;
+	VideoCapture capture(0);
+	
+	cout << "Viewer opened" << endl
+		 << "Press ESC to terminate" << endl;		
+	// While the quit fucntion does not return true run image functions
+	while (!quit()) {
+		//Stores capture to ram mat
+		capture.read(raw);
+		//Runs image processing - give mats raw and proccessed
+		processImage(raw, processed);
+		// show processed image
+		imshow("Processed image", processed);
+	}
+	cout << "Viewer closed successfully";
 	
 	return 0;
 }
