9ab63ebc9ac77f74e96d29dd36102df949848293
Tayler Uva <tayleruva@gmail.com>
2017-06-18 00:11:00 -0700

    Targeting now writing as a function
    Renamed variables Made overall feel more like NURDVision

--- NURDVision3.0.cpp
+++ NURDVision3.0.cpp
@@ -22,7 +22,9 @@ using namespace cv;
 const double Hue[] = {0, 49};
 const double Saturation[] = {0, 70};
 const double Luminance[] = {245, 255};
+const int KNOWN_AREA = 5000; //At 10 inches
 // =================================================//
+bool lockAcquired = false;
 
 // Converts image to hsl filter, filtering out any color besides retroreflective tapes
 void hslThreshold(Mat &input, const double hue[], const double sat[], const double lum[], Mat &output) {
@@ -40,8 +42,8 @@ void createMask(Mat &input, Mat &mask, Mat &output) {
 // Filters out contours 
 void filterContours(vector<vector<Point> > &input, vector<vector<Point> > &output) {
 	output.clear();
-	for (vector<cv::Point> contour : input) {
-		cv::Rect bb = boundingRect(contour);
+	for (vector<Point> contour : input) {
+		Rect bb = boundingRect(contour);
 		if (bb.width < 6 || bb.width > 1000000) continue;
 		if (bb.height < 6 || bb.height > 1000000) continue;
 		double area = contourArea(contour);
@@ -51,23 +53,8 @@ void filterContours(vector<vector<Point> > &input, vector<vector<Point> > &outpu
  	}
 }
 
-
-
-// IMPORTED *******
-Point2f centerPoint(cv::Rect rect) {
-	return cv::Point2f(rect.x + (rect.width / 2), rect.y + (rect.height / 2));
-}
-double aimCoords(double pos, double res) {
-		return (pos - (res / 2)) / (res / 2);
-}
-
-Point2f aimCoordsFromPoint(cv::Point2f point, cv::Size res) {
-		return Point2f(aimCoords(point.x, (double)res.width), aimCoords(point.y, (double)res.height));
-}
-// IMPORTED *******
-
 // Creates contours
-void createContours(Mat &input, Mat &output, vector<vector<Point> > &contoursOutput){
+void createContours(Mat &input, vector<vector<Point> > &contoursOutput){
 	//makes vectors to store contour information
 	vector<vector<Point> > contoursInput;
 	vector<Vec4i> hierarchy;
@@ -76,34 +63,74 @@ void createContours(Mat &input, Mat &output, vector<vector<Point> > &contoursOut
 	//finds the contours of the image
 	findContours(input, contoursInput, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 	//creates mat for the drawing of the contours
-	Mat drawing = Mat::zeros(input.size(), CV_8UC3);
-	
 	filterContours(contoursInput, contoursOutput);
-	
-	
-	Scalar color = Scalar(250, 206, 135);
-	for (int i = 0; i< contoursOutput.size(); i++)
-	{
-		drawContours(drawing, contoursOutput, i, color, 2);
-		
-//* ************ COPIED ************** *//
+}
+
+// Targeting Info //
+// Finds Center point
+Point2f centerPoint(Rect rect) {
+	return Point2f(rect.x + (rect.width / 2), rect.y + (rect.height / 2));
+}
+// Finds offset from center point
+double findOffset(double position, double resolution) {
+		return (position - (resolution / 2)) / (resolution / 2);
+}
+// Finds x and y offsets
+Point2f findXYOffset(Point2f point, Size res) {
+		return Point2f(findOffset(point.x, (double)res.width), findOffset(point.y, (double)res.height));
+}
+// Shows crosshairs
+void showCrosshairs(Mat &input){
+	line(input, Point(input.cols / 2, 0), Point(input.cols / 2, input.rows), (lockAcquired ? Scalar(0, 255, 0) : Scalar(0, 0, 255)), 1);
+	line(input, Point(0, input.rows / 2), Point(input.cols, input.rows / 2), (lockAcquired ? Scalar(0, 255, 0) : Scalar(0, 0, 255)), 1);
+}
+
+// Finds targets
+void findTargets(Mat &imageInput, vector<vector<Point> > &input, Mat &output){	
+	output = Mat::zeros(imageInput.size(), CV_8UC3);
+	for (int i = 0; i< input.size(); i++) {
+		// Creates Bounding Boxes //
+		// Bounding rectangles around contours
+		vector<Rect> rects(input.size());
+		// Rotated bounding rectangles around contours
+		vector<RotatedRect> rotRects(input.size());
 		// Adds rectangles
+		Rect conRect = boundingRect(input[i]);
 		rects[i] = conRect;
+		rotRects[i] = minAreaRect(input[i]);
 		
+		Scalar contourColor = Scalar(250, 206, 135);
+		Scalar rectangleColor = Scalar(255, 255, 0);
+		drawContours(output, input, i, contourColor, 2);
+		rectangle(output, conRect, rectangleColor, 2);
+		
+		// Creates aimPoints
+		Point2f offset = findXYOffset(centerPoint(conRect), output.size());
+		
+		// Changes lockAcquired if the target is locked on
+		(-0.1 < offset.x < 0.1 && -0.1 < offset.y < 0.1) ? lockAcquired = true : lockAcquired = false; //TODO: Do better target aquition
+		
+		Scalar textColor = Scalar(255, 255, 255);
 		
 		double area = conRect.area();
-//* ************ COPIED ************** *//
-
+		double distance = (area/KNOWN_AREA)*10;
+		putText(output, "Angle X: "+ to_string(offset.x), centerPoint(conRect), FONT_HERSHEY_PLAIN, 1, textColor, 1);
+		putText(output, "Angle Y: "+ to_string(offset.y), centerPoint(conRect) + Point2f(0, 15), FONT_HERSHEY_PLAIN, 1, textColor, 1);
+		putText(output, "Distance: " + to_string(distance), centerPoint(conRect) + Point2f(0, 30), FONT_HERSHEY_PLAIN, 1, textColor, 1);
 	}
-	output = drawing;
 }
 
 // Does the image processing
 void processImage(Mat& input, Mat& output){
+	Mat hslOutput, maskOutput, contoursImageOutput, targetsOutput;
 	vector<vector<Point> > contoursValueOutput;
+	
 	hslThreshold(input, Hue, Saturation, Luminance, hslOutput);
 	createMask(input, hslOutput, maskOutput);
-	createContours(maskOutput, contoursImageOutput, contoursValueOutput);
+	createContours(maskOutput, contoursValueOutput);
+	findTargets(maskOutput, contoursValueOutput, targetsOutput);
+	output = targetsOutput;
+	showCrosshairs(output);
 }
 
 // Returns true to quit when "ESC" is pressed
