f83c93f5feeab83be12e6cecbabb51b533d7b511
Tayler Uva <tayleruva@gmail.com>
2017-10-06 19:52:13 -0700

    Updated runscript.sh

--- runscript.sh
+++ runscript.sh
@@ -1,3 +1,4 @@
-#! /bin/bash                                                                    
+#! /bin/bash
+v4l2-ctl -c exposure_auto=(camera to change exposure of, most of the time 0 or 1) -c exposure_absolute=01                                                                    
 cd ~/Desktop/
 ./runNURDVision
