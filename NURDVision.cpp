ce9361547897d9b43c937333facb3904a15dff77
Tayler Uva <tayleruva@gmail.com>
2017-11-13 15:45:34 -0800

    Implemented config.h
    Added a config file for modifying values and parameters of the program.
    Improved main code by changing functions to return values rather than
    pass in output parameters

--- NURDVision.cpp
+++ NURDVision.cpp
@@ -8,61 +8,34 @@
 #include <networktables/NetworkTable.h>
 #include <ntcore.h>
 #include <cscore.h>
-
-//'namespaces' are used basically before each command, so 'createMask' is actually cv::createMask. This removes need to do so.
-using namespace std;
-using namespace cv;
+#include "config.h"
 using namespace cs;
 
-// FIRST Robotics Competition team number
-const int teamNumber = 3255;
-// Camera input (default is 0 for jetson use)
-const int camerainput = 1;
-// Port number of the stream (on jetson available at tegra-ubuntu.local:"streamPort" being the port below)
-//		should be on the ip address of the device thats doing the processing, in our case the Jetson.
-const int streamPort = 1180;
+// ============== GLOBAL VARIABLES ============== //
+// Initalizes targetDistance, targetAngle, and targetOffset to 0.0;
+double targetDistance = 0.0;
+double targetAngle = 0.0;
+double targetOffset = 0.0;
 
-// Store a double array for both lower and upper boundaries of the hsl filter, decides what color you're looking for in the first mask
-// ========= Constants for Tape tracking ============//
-// Store an array: [0] = lower bound, [1] = upper bound
-//const double Hue[] = {0, 49};
-//const double Saturation[] = {150, 255};
-//const double Luminance[] = {245, 255};
+// Initalizes local, debug, and showRaw to false
+bool local = false;
+bool debug = false;
+bool showRaw = false;
+// ============================================== //
 
-//const double OBJECT_AREA = 2.5; //Area of the tracking tape in real life
-//const double PIXEL_AREA = 30000; //Area of the pixels at 10 inches
-//const double BASE_DISTANCE = 10; //Distance from camera to use as base for calucation
-// =================================================//
+// Checks the parameters passed by the command line execution
+void checkRunParameters(int argc, char *argv[]){
+	if(argc >= 2){
+		if(string(argv[1]) == "-local") local=true;
+		if(string(argv[1]) == "-debug") debug=true;
+	}
+	if(argc >= 3){
+		if(string(argv[2]) == "-local") local=true;
+		if(string(argv[2]) == "-debug") debug=true;
+	}
+}
 
-// ========= Constants for SykoraBot ============//
-// Store an array: [0] = lower bound, [1] = upper bound
-//My laptop values
-//HSL VALUES FOR CAMERA WITH EXPOSURE SET AT 20; PASTE THIS COMMAND IN TO CHANGE EXPOSURE 'v4l2-ctl -c exposure_auto=(camera to change exposure of, most of the time 0 or 1) -c exposure_absolute=20' 
-vector<double> Hue = {47, 96};
-vector<double> Saturation = {186, 255};
-vector<double> Luminance = {16, 129};
-//Asks if it should show the Raw Image over Networktables
-//
-const double OBJECT_AREA = 18.5; //Area of the tracking tape in real life
-const double OBJECT_WIDTH = 2;
-const double PIXEL_AREA = 8190; //Area of the pixels at base distance
-const double BASE_DISTANCE = 36; //Distance from camera to use as base for calucation
-const double PIXEL_WIDTH = 40;
-// =================================================//
-
-// ============ Color Constants ============ //
-// Scalars writen in BGR format
-const Scalar SKY_BLUE = Scalar(250, 206, 135);
-const Scalar RED = Scalar(0, 0, 255);
-const Scalar GREEN = Scalar(0, 255, 0);
-const Scalar BLUE = Scalar(255, 0, 0);
-const Scalar YELLOW = Scalar(0, 255, 255);
-const Scalar WHITE = Scalar(255, 255, 255);
-// ========================================= //
-
-//Focal length for camera/
-const double FOCAL_LENGTH = (PIXEL_WIDTH * BASE_DISTANCE)/OBJECT_WIDTH;
-
+// ============== Filtering ============== //
 // Structure for sorting bounding boxes in descending size order
 struct {
 	bool operator() (cv::Rect a, cv::Rect b) {
@@ -70,10 +43,10 @@ struct {
 	}
 } boundingBoxSortDescending;
 
-// Converts image to hsl filter, filtering out any color besides retroreflective tapes
-void hslThreshold(Mat &input, vector<double>  hue, vector<double>  sat, vector<double>  lum, Mat &output) {
+// Converts image to hsl filter, filtering out any color besides green on the retroreflective tapes
+void hslThreshold(Mat &input, Mat &output) {
 	cvtColor(input, output, COLOR_BGR2HLS);
-	inRange(output, Scalar(hue[0], lum[0], sat[0]), Scalar(hue[1], lum[1], sat[1]), output);
+	inRange(output, Scalar(hue[0], luminance[0], saturation[0]), Scalar(hue[1], luminance[1], saturation[1]), output);
 }
 
 // Creates a workable mask to a mat, so we can process the hsl filter
@@ -88,40 +61,45 @@ void filterContours(vector<vector<Point> > &input, vector<vector<Point> > &outpu
 	output.clear();
 	for (vector<Point> contour : input) {
 		Rect contourBounding = boundingRect(contour);
-		if (contourBounding.width < 6 || contourBounding.width > 1000000) continue;
-		if (contourBounding.height < 6 || contourBounding.height > 1000000) continue;
-		double area = contourArea(contour);
-		if (area < 50) continue;
-		if (arcLength(contour, true) < 100) continue;
+		if (contourBounding.width < contourWidth[0] || contourBounding.width > contourWidth[1]) continue;
+		if (contourBounding.height < contourHieght[0] || contourBounding.height > contourHieght[1]) continue;
+		if (contourArea(contour) < minContourArea) continue;
+		if (arcLength(contour, true) < minContourArcLength) continue;
 		output.push_back(contour);
  	}
 }
 
-// Creates contours
-void createContours(Mat &input, vector<vector<Point> > &contoursOutput){
-	//makes vectors to store contour information
-	vector<vector<Point> > contoursInput;
-	//converts our hsl filter into black and white, because findContours requires you to be using black and white, not RGB or HSL	
+// Creates contours and returns a vector of contour points
+vector<vector<Point> > createContours(Mat &input){
+	// Create new vectors for storing contour values
+	vector<vector<Point> > contoursInput, contoursOutput;
+	// Converts our hsl filter into black and white, because findContours requires you to be using black and white, not RGB or HSL	
 	cvtColor(input, input, CV_RGB2GRAY);
-	//finds the contours of the image
+	// Finds the contours of the image
 	findContours(input, contoursInput, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-	//creates mat for the drawing of the contours
+	// Filters out contours based on width, height, and area.
 	filterContours(contoursInput, contoursOutput);
+	return contoursOutput;
 }
+// ============== Filtering END ============== //
 
-// Targeting Info //
+
+// ============== Targeting ============== //
 // Finds Center point
 Point2f centerPoint(Rect rect) {
 	return Point2f(rect.x + (rect.width / 2), rect.y + (rect.height / 2));
 }
-// Finds offset from center point
+
+// Finds offset from center point and returns is
 double findOffset(double position, double resolution) {
 	return (position - (resolution / 2)) / (resolution / 2);
 }
-// Finds x and y offsets
+
+// Finds x and y offsets and returns them
 Point2f findXYOffset(Point2f point, Size res) {
 	return Point2f(findOffset(point.x, (double)res.width), findOffset(point.y, (double)res.height));
 }
+
 // Shows crosshairs
 void showCrosshairs(Mat &input){
 	line(input, Point(input.cols / 2, 0), Point(input.cols / 2, input.rows), WHITE, 1);
@@ -129,7 +107,7 @@ void showCrosshairs(Mat &input){
 }
 
 // Finds targets
-void findTargets(Mat &imageInput, vector<vector<Point> > &input, Mat &output, double &avgDistance, double &angle, double &offset){	
+void findTargets(Mat &imageInput, vector<vector<Point> > &input, Mat &output){	
 	// Readys Output mat for contour use
 	output = Mat::zeros(imageInput.size(), CV_8UC3);
 	// Creates Vector of Bounding Boxes
@@ -137,15 +115,13 @@ void findTargets(Mat &imageInput, vector<vector<Point> > &input, Mat &output, do
 	
 	// Finds all the targets
 	for (int i = 0; i< input.size(); i++) {
-		// Adds rectangles
-		Rect contourBounding = boundingRect(input[i]);
-		// Stores rectangles into bounding box vector
-		boundingBoxes[i] = contourBounding;
-		//Draws contours onto output
+		// Makes contours rectanges and stores rectangles into bounding box vector.
+		boundingBoxes[i] = boundingRect(input[i]);
+		// Draws contours onto output
 		drawContours(output, input, i, SKY_BLUE, 2);
 	}
 	
-	// If there are more than 2 targets found
+	// If there are 2 or more targets found
 	if (boundingBoxes.size() >= 2) {
 		// Sort rectangles in descending order based on their area.
 		sort(boundingBoxes.begin(), boundingBoxes.end(), boundingBoxSortDescending); 
@@ -167,78 +143,71 @@ void findTargets(Mat &imageInput, vector<vector<Point> > &input, Mat &output, do
 		Point2f tl(midPoint.x - 2, midPoint.y - 2);	// Calculate top-left point of center dot
 		Point2f tr(midPoint.x + 2, midPoint.y + 2); // Calculate bottom right point of center dot
 		
-		
-		
 		// Draw a line between center point of vision targets
 		line(output, centerPoint1, centerPoint2, RED, 1); 
 		// Draw a point at center of line drawn above
 		rectangle(output, tl, tr, BLUE, 3);
 		
-		// Distance and angle calculation
-		Point2f midPointNormal = findXYOffset(midPoint, output.size());
+		// Distance, angle, and offset calculation
+		Point2f offCenter = findXYOffset(midPoint, output.size()); // Calculates x and y offset from center
 		double target1Width = target1.width;
 		double target2Width = target2.width;
 		double target1Height = target1.height;
 		double target2Height = target2.height;
-		double metricDistance = ((OBJECT_WIDTH * FOCAL_LENGTH)/(target1Width+target2Width))/2;
-		avgDistance = 3.9370*metricDistance;
-		offset = -midPointNormal.x;
+		double metricDistance = ((OBJECT_WIDTH * FOCAL_LENGTH)/(target1Width+target2Width))/2; // This is coincidentally in decameters
+		targetDistance = 3.9370*metricDistance; // Target's distance from robot (inches)
+		targetOffset = -offCenter.x; // Target's horizontal offset from center (no units)
 
 		if (centerPoint1.x > midPoint.x){
-			angle = target1Height - target2Height;	
+			targetAngle = target1Height - target2Height;	
 	    }
 		else{
-			angle = target2Height - target1Height;
+			targetAngle = target2Height - target1Height;
 		}
+		
 		// Put text on image (used for debugging)
 		putText(output, "Final Target Data (Blue Dot):", Point2f(15, 1*15), FONT_HERSHEY_PLAIN, 0.8, WHITE, 1);
-		putText(output, "Angle: "+ to_string(angle), Point2f(30,2*15), cv::FONT_HERSHEY_PLAIN, 0.8, WHITE, 1);
-		putText(output, "Avg Distance: "+ to_string(avgDistance), Point2f(30,3*15), FONT_HERSHEY_PLAIN, 0.8, WHITE, 1);
+		putText(output, "Angle: "+ to_string(targetAngle), Point2f(30,2*15), cv::FONT_HERSHEY_PLAIN, 0.8, WHITE, 1);
+		putText(output, "Distance: "+ to_string(targetDistance), Point2f(30,3*15), FONT_HERSHEY_PLAIN, 0.8, WHITE, 1);
 		
 		putText(output, "Debug Data:", Point2f(15, 5*15), FONT_HERSHEY_PLAIN, 0.8, WHITE, 1);
 	}
 }
 
-// Does the image processing
-void processImage(Mat& input, Mat& output,Mat& hslOutput , double &distance, double &angle, double &offset){
+// ============== Targeting END ============== //
+
+// Processes the image and returns a processed image
+Mat processImage(Mat &input){
 	// Mats for processed outputs
-	Mat maskOutput, contoursImageOutput, targetsOutput;
+	Mat hslOutput, maskOutput, contoursImageOutput, targetsOutput;
 	// Create a HSL Mask
-	hslThreshold(input, Hue, Saturation, Luminance, hslOutput);
+	hslThreshold(input, hslOutput);
 	createMask(input, hslOutput, maskOutput);
 	
-	// Vector for storing contour values
-	vector<vector<Point> > contoursValueOutput;
 	// Create Contours
-	createContours(maskOutput, contoursValueOutput);
+	auto contoursValueOutput = createContours(maskOutput);
 	
-	// Finds targets and passes back distance and angle
-	findTargets(maskOutput, contoursValueOutput, targetsOutput, distance, angle, offset);
-	output = targetsOutput;
+	// Finds targets and passes back distance and targetAngle
+	findTargets(maskOutput, contoursValueOutput, targetsOutput);
 	
 	// Draw crosshairs on image
-	showCrosshairs(output);
+	showCrosshairs(targetsOutput);
+	return targetsOutput;
 }
 
-//Initalize Network Tables to team 3255 and returns table to use
-shared_ptr<NetworkTable> InitalizeNetworkTables(string tableName,int frcTeamNumber) {
-	NetworkTable::SetClientMode();
-	NetworkTable::SetTeam(frcTeamNumber);
-	NetworkTable::Initialize();
-	return NetworkTable::GetTable(tableName);
+// Publish Network Tables to table in use
+void PublishNetworkTables(shared_ptr<NetworkTable> table) {
+	table->PutNumber("Distance", targetDistance);
+	table->PutNumber("Angle", targetAngle);
+	table->PutNumber("Offset", targetOffset);
 }
 
-//Publish Network Tables to table in use
-void PublishNetworkTables(shared_ptr<NetworkTable> table, double distance,double angle, double offset) {
-	table->PutNumber("Distance", distance);
-	table->PutNumber("Angle", angle);
-	table->PutNumber("Offset", offset);
-}
-
-void GetNetworkTables(shared_ptr<NetworkTable> table) {
-	Hue = table->GetNumberArray("hue", Hue);
-	Saturation = table->GetNumberArray("sat", Saturation);
-	Luminance = table->GetNumberArray("lum", Luminance);
+// Get values from for Hue, Saturation, Luminance from NetworkTables
+void GetHSLValues(shared_ptr<NetworkTable> prefTable) {
+	hue = prefTable->GetNumberArray("hue", hue);
+	saturation = prefTable->GetNumberArray("sat", saturation);
+	luminance = prefTable->GetNumberArray("lum", luminance);
+	showRaw = prefTable->GetBoolean("showRaw", false);
 }
 
 // Returns true to quit when "ESC" is pressed
@@ -247,91 +216,83 @@ bool quit(){
 	return( key == 27 || key == 'q' || key == 'Q' ); // key 27 = 'ESC'
 }
 
-int main(int argc, char *argv[]) {
+// Displays welcome messages and debug info
+void greetings(){
 	cout << "\e[1mRunning \e[34mNURD\e[31mVision\e[0m \tFRCTeam 3255 SuperNURD 2017 Vision Processing\n" << 
-	"\t\t\tCreated by Mike Smith & Tayler Uva\n\n";
-					
-	// Checks run arguments
-	bool local=false;
-	bool debug=false;
-	if(argc >= 2){
-		if(string(argv[1]) == "-local") local=true;
-		if(string(argv[1]) == "-debug") debug=true;
-	}
-	if(argc >= 3){
-		if(string(argv[2]) == "-local") local=true;
-		if(string(argv[2]) == "-debug") debug=true;
-	}
+		"\t\t\tCreated by Mike Smith & Tayler Uva\n\n";
+	
 	cout << "Debug: " << (debug ? "\e[32mtrue\e[0m" : "\e[31mfalse\e[0m") << endl;
 	cout << "localhost: " << (local ? "\e[32mtrue\e[0m" : "\e[31mfalse\e[0m") << endl;
 	cout << "Team Number: " << "\e[34m" << teamNumber << "\e[0m" << endl;
 	cout << endl;
-				
-	// Initalizes distance and angle to 0.0;
-	double distance = 0.0;
-	double angle = 0.0;
-	double offset = 0.0;
 	
+	cout << "\nSTARTING IMAGE PROCESSING\n" << endl;
+		
+	cout << "MJpeg stream available at port " << (streamPort) << endl;
+		
+	if(debug){
+	cout << "\nVIEWER OPENED" << endl
+		 << "Press ESC or Q to terminate\n" << endl;		
+	}
+}
+
+int main(int argc, char *argv[]) {
+						
+	// Checks run arguments
+	checkRunParameters(argc, argv);
+
+	// Displays welcome messages and debug info
+	greetings();
+		
 	// Initalizes camera stream
-	CvSource stream = CvSource("stream", VideoMode::PixelFormat::kMJPEG, 640, 480, 30);
+	CvSource stream = CvSource("stream", VideoMode::PixelFormat::kMJPEG, resolutionWidth, resolutionHeight, maxFrameRate);
 	MjpegServer streamServer = MjpegServer("server", streamPort);
 	streamServer.SetSource(stream);
 	
-	//Initalizes Networktables
-	shared_ptr<NetworkTable> visionTable = InitalizeNetworkTables("NURDVision", teamNumber);
-	shared_ptr<NetworkTable> preferenceTable = NetworkTable::GetTable("Preferences");
-	// If run argument -local is present, set ip address to localhost
-	if(local) NetworkTable::SetIPAddress("localhost");
+	//Initalizes NetworkTables
+	NetworkTable::SetClientMode();
+	NetworkTable::SetTeam(teamNumber);
+	NetworkTable::Initialize();
+	auto visionTable = NetworkTable::GetTable("NURDVision");
+	auto preferenceTable = NetworkTable::GetTable("Preferences");
+		// If run argument -local is present, set ip address to localhost
+		if(local) NetworkTable::SetIPAddress("localhost");
 
 	// Creates mats for storing image
-	Mat raw, processed, hslOutput;
+	Mat raw, processed;
+
 	// Starts video capture of camera 0;
-	VideoCapture capture(camerainput);
+	VideoCapture capture(cameraInput);
 	if (!capture.isOpened()) {
 		cerr << "\n\e[31mERROR! Unable to open camera\nERROR! Is the camera connected?\e[0m\n";
 		NetworkTable::Shutdown();
 		return 1;
 	}
 	
-	cout << "\nSTARTING IMAGE PROCESSING\n" << endl;
-	
-	cout << "MJpeg stream available at port " << (streamPort) << endl;
-	
-	if(debug){
-	cout << "VIEWER OPENED" << endl
-		 << "Press ESC or Q to terminate\n" << endl;		
-	}
-
-	// While the quit fucntion does not return true run image functions
+	// While the quit function does not return true run image functions
 	while (!quit()) {
 		// Stores capture to raw mat
 		capture.read(raw);
-		// Runs image processing - pass mats raw, returns and stores mat processed, doubles distance and angle
-		processImage(raw, processed, hslOutput, distance, angle, offset);
+		
+		// Runs image processing and stores to processed
+		processed = processImage(raw);
+		
 		// Publishes Data to NetworkTable - Vision
-		PublishNetworkTables(visionTable, distance, angle, offset);
+		PublishNetworkTables(visionTable);
+		
 		// Gets array values for Hue Saturation and Luminance from preferences table
-		GetNetworkTables(preferenceTable);
-		//Checks robot preferences to see if raw images is called for
-		bool showRaw = preferenceTable->GetBoolean("showRaw", false);
-		// Publishes processed image to stream
-		if(showRaw==true){
-			stream.PutFrame(raw);
-		}
-		else{
-			stream.PutFrame(processed);
-		}
-
-		cout << "\tHue" << Hue[0] << Hue[1] << endl;
-
+		GetHSLValues(preferenceTable);
+		
+		// Publishes processed image to stream (checks to see if asking for raw)
+		(showRaw ? stream.PutFrame(raw) : stream.PutFrame(processed));
+	
 		// Runs if debug is true
 		if(debug){
 			// Display processed image
 			imshow("Processed image", processed);
 			imshow("Raw Image", raw);
-			imshow("HSL Image", hslOutput);
 			// Output data to console
-			cout << "Distance: "<< distance << "\tAngle: " << angle << "\tOffset: " << offset <<  endl;
+			cout << "Distance: "<< targetDistance << "\tAngle: " << targetAngle << "\tOffset: " << targetOffset <<  endl;
 		}
 	}
 	NetworkTable::Shutdown();
---
+++ config.h
@@ -0,0 +1,64 @@
+// ===========================================================
+// NURDVision Configuration File
+// Use this file to modify settings that can change team to
+// team or season to season
+// ============== DO NOT MODIFY THIS SECTION =================
+	using namespace std;
+	using namespace cv;
+// ===========================================================
+
+// ============== Basic Settings ============== //
+const int teamNumber = 3255;	// FIRST Robotics Competition team number
+const int cameraInput = 0;		// Camera input (default is 0 for jetson use)
+const int streamPort = 1180;	// Port number of the video stream
+								// 	(on jetson available at tegra-ubuntu.local:"streamPort" being the port below)
+								// 	should be on the ip address of the device thats doing the processing, in our case the Jetson.
+// ============================================ //
+								
+// ============== Stream Settings ============= //
+int resolutionWidth = 640;		// Horizontal Resolution for the camera stream
+int resolutionHeight = 480;		// Vertical Resolution for the camera stream
+int maxFrameRate = 30;			// Maximum frame rate for the camera strea
+// Please note: The FRC Field allows only limited bandwith per robot, rasing these values may go past that bandwith
+// ============================================ //							
+
+// ======== Tracking Tape Caliberation ======== //
+const double OBJECT_WIDTH = 2; 	// Width (inches) of the tracking tape in real life
+const double BASE_DISTANCE = 36;// Distance (inches) from camera to use as base for calucation
+const double PIXEL_WIDTH = 40; 	// Width (inches) of the pixels at base distance
+
+
+const double FOCAL_LENGTH = 	//Focal length for camera
+	(PIXEL_WIDTH * BASE_DISTANCE)/OBJECT_WIDTH; 
+	// (this can be replaced with a value if your camera's focal length is known
+// ============================================ //
+	
+// ========= Contour Filter Threshold ========= //
+// Lower and upper bound (in that respective order) threshold for contour filtering
+int contourWidth[] = {6, 1000000};
+int contourHieght[] = {6, 1000000};
+
+int minContourArea = 50;
+int minContourArcLength = 100;
+// ============================================ //
+
+// ============ Default HSV Values ============ //
+// Use these if you would like to code the values on the image processor rather than receive them from the RoboRIO
+// Store a double vector for both lower and upper bounds (in that respective order) of the hsl filter
+//	decides what color you're looking for in the first mask
+vector<double> hue = {47, 96};
+vector<double> saturation = {186, 255};
+vector<double> luminance = {16, 129};
+// ============================================ //
+
+// ============= Color Constants ============== //
+// You don't need to touch these 
+//	unless you want additional colors.
+// Scalars writen in BGR format
+const Scalar SKY_BLUE = Scalar(250, 206, 135);
+const Scalar RED = Scalar(0, 0, 255);
+const Scalar GREEN = Scalar(0, 255, 0);
+const Scalar BLUE = Scalar(255, 0, 0);
+const Scalar YELLOW = Scalar(0, 255, 255);
+const Scalar WHITE = Scalar(255, 255, 255);
+// =========================================== //
