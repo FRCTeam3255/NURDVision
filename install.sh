8b775ac0c33f31659a8c9bce6a7681999aebc41b
Tayler Uva <tayleruva@gmail.com>
2017-11-13 22:52:16 -0800

    Updated install scripts and nurdcommands to account for config.h

--- install.sh
+++ install.sh
@@ -94,6 +94,7 @@ cd
 cd ~/Desktop
 rm NURDVision.cpp
 wget https://github.com/FRCTeam3255/NURDVision/raw/master/NURDVision.cpp
+wget https://github.com/FRCTeam3255/NURDVision/raw/master/config.h
 g++ NURDVision.cpp -std=c++11 -lopencv_calib3d -lopencv_core -lopencv_features2d -lopencv_flann -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_ml -lopencv_objdetect -lopencv_photo -lopencv_shape -lopencv_stitching -lopencv_superres -lopencv_video -lopencv_videoio -lopencv_videostab -lntcore -lwpiutil -lcscore -o runNURDVision
 #CODE SETUP DONE
 
