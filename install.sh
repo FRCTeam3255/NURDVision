df85043496e86b79ad9923dffe5bf825c35e8558
Mike Smith <mssmichaelsmith@gmail.com>
2018-02-16 13:38:08 -0800

    added cascade.xml

--- install.sh
+++ install.sh
@@ -98,6 +98,7 @@ cd ~/Desktop
 rm NURDVision.cpp
 wget https://github.com/FRCTeam3255/NURDVision/raw/master/NURDVision.cpp
 wget https://github.com/FRCTeam3255/NURDVision/raw/master/config.h
+wget https://github.com/FRCTeam3255/NURDVision/raw/master/cascade.xml
 g++ NURDVision.cpp -std=c++11 -lopencv_calib3d -lopencv_core -lopencv_features2d -lopencv_flann -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_ml -lopencv_objdetect -lopencv_photo -lopencv_shape -lopencv_stitching -lopencv_superres -lopencv_video -lopencv_videoio -lopencv_videostab -lntcore -lwpiutil -lcscore -o runNURDVision
 #CODE SETUP DONE
 
